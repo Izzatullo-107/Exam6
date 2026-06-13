1d36ada192a9f8b07efe2598775cfd05e0fc8833
Izzatullo-107 <107.izzatullo@example.com>
2026-06-13 16:47:28 +0500

    WI7-WI8

---
+++ ExamSolution/src/Exam1/Repositories/IBaseRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Exam1.Repositories
+{
+    public interface IBaseRepository<T>
+    {
+        Task AddAsync(T t);
+        void Update(T t);
+        void Delete(T t);
+        Task SaveChangesAsync();
+    }
+}
