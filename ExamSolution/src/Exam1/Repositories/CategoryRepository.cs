abb5d3a9c013c66d9a77bba79922f3346509dbf1
Izzatullo-107 <107.izzatullo@example.com>
2026-06-13 17:03:52 +0500

    wi7-wi8

---
+++ ExamSolution/src/Exam1/Repositories/CategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using Exam1.Data;
+using Exam1.Entities;
+
+namespace Exam1.Repositories
+{
+    public class CategoryRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllCategoriesAsync()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category> CreateCategoryAsync(Category category)
+        {
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+    }
+}
---
+++ ExamSolution/src/Exam1/Repositories/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using Exam1.Entities;
+
+namespace Exam1.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAllCategoriesAsync();
+        Task<Category> CreateCategoryAsync(Category category);
+    }
+}
