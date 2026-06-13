b6c21cad667bfecacebaa5fcb764aea8d2d04653
Erkinbek25 <erkinbekhasanov1@gmail.com>
2026-06-13 17:46:41 +0500

    WI 10 food service added

---
+++ ExamSolution/src/Exam1/Services/FoodService.cs
@@ -0,0 +1,32 @@
+﻿using Exam1.Dtos;
+using Exam1.Mappings;
+using Exam1.Repositories;
+
+namespace Exam1.Services;
+
+public class FoodService : IFoodService
+{
+    private readonly IFoodRepository _foodRepository;
+    private readonly IFoodMapper _foodMapper;
+
+    public FoodService(IFoodRepository foodRepository, IFoodMapper foodMapper)
+    {
+        _foodRepository = foodRepository;
+        _foodMapper = foodMapper;
+    }
+
+    public async Task<IEnumerable<FoodGetDto>> GetAllAsync()
+    {
+        var foods = await _foodRepository.GetAllFoodsAsync();
+        return _foodMapper.ToGetDto(foods.ToList());
+    }
+
+    
+    public async Task CreateAsync(FoodCreateDto dto)
+    {
+        var food = _foodMapper.ToEntity(dto);
+        await _foodRepository.CreateFoodAsync(food);
+    }
+
+   
+}
---
+++ ExamSolution/src/Exam1/Services/IFoodService.cs
@@ -0,0 +1,11 @@
+﻿using Exam1.Dtos;
+
+namespace Exam1.Services;
+
+public interface IFoodService
+{
+    Task<IEnumerable<FoodGetDto>> GetAllAsync();
+   
+    Task CreateAsync(FoodCreateDto dto);
+   
+}
