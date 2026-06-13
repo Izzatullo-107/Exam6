3f244a7231dc4be18082bb2d4b18914b7a98a2b9
Izzatullo-107 <107.izzatullo@example.com>
2026-06-13 15:39:33 +0500

    WI2

---
+++ ExamSolution/src/Exam1/Entities/Food.cs
@@ -0,0 +1,12 @@
+﻿namespace Exam1.Entities;
+
+public class Food
+{
+    public long FoodId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public bool IsAvailable { get; set; }
+    public long CategoryId { get; set; }
+    public Category Category { get; set; }
+}
