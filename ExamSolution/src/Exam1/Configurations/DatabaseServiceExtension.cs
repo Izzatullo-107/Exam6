614021dcc28233de44909053b7b177b22927bab2
Erkinbek25 <erkinbekhasanov1@gmail.com>
2026-06-13 16:09:48 +0500

    WI3 — EF Core va SQL Server bajarildi

---
+++ ExamSolution/src/Exam1/Configurations/DatabaseServiceExtension.cs
@@ -0,0 +1,20 @@
+﻿
+using Exam1.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Exam1.Configurations;
+
+public static class DatabaseServiceExtension
+{
+    public static IServiceCollection AddDatabaseService(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        return services;
+    }
+}
+
