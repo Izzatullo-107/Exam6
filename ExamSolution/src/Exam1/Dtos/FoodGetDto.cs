161177f1a4513dbe5ae6c52cb0b2f3df34c612f8
maftunamaqsudova <maftuna.starr@gmail.com>
2026-06-13 16:10:19 +0500

    Dtos added

---
+++ ExamSolution/src/Exam1/Dtos/FoodGetDto.cs
@@ -0,0 +1,20 @@
+﻿using Exam1.Entities;
+
+namespace Exam1.Dtos
+{
+    public class FoodGetDto
+    {
+        public long FoodId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public long  CategoryId { get; set; }
+        public Category? Category { get; set; }
+    }
+}
