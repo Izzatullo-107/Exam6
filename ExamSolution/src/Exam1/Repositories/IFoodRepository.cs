98327baf808674f2ee0bc2a10d5ea7828dbf4491
Izzatullo-107 <107.izzatullo@example.com>
2026-06-13 17:23:39 +0500

    wi8

---
+++ ExamSolution/src/Exam1/Repositories/IFoodRepository.cs
@@ -0,0 +1,10 @@
+﻿using Exam1.Entities;
+
+namespace Exam1.Repositories
+{
+    public interface IFoodRepository
+    {
+        Task<IEnumerable<Food>> GetAllFoodsAsync();
+        Task<Food> CreateFoodAsync(Food food);
+    }
+}
